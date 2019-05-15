bd49d06659cc62d1431998e3bd601f547b46f8f4
CaoZheng <36647533+caozhengisnotershiu@users.noreply.github.com>
2019-05-15 16:09:16 +0800

    刚创建ssm项目

--- src/main/java/net/jiguo/controller/GuidController.java
+++ src/main/java/net/jiguo/controller/GuidController.java
@@ -1,5 +1,6 @@
 package net.jiguo.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @RequestMapping(value = "guid")
+@Api(value = "GuidController", tags = "guid接口")
 public class GuidController {
 
     @RequestMapping(value = "index",method = RequestMethod.GET)
